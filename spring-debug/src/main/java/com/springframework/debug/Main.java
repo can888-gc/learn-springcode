907657734fc432f47ea92963a575cc6d7db5b2a8
sangguangcan <sanggc@beiktech.com>
2022-12-12 21:50:50 +0800

    删除测试文件中无用的代码

--- spring-debug/src/main/java/com/springframework/debug/Main.java
+++ spring-debug/src/main/java/com/springframework/debug/Main.java
@@ -7,11 +7,6 @@ import java.util.Properties;
 
 public class Main {
 	public static void main(String[] args) {
-//		ClassPathXmlApplicationContext ap = new ClassPathXmlApplicationContext("application.xml");
-//		Person person = (Person)ap.getBean("person");
-//		System.out.println("111");
-		Properties properties = System.getProperties();
-		Map<String, String> getenv = System.getenv();
-		System.out.println("1111");
+		ClassPathXmlApplicationContext ap = new ClassPathXmlApplicationContext("application.xml");
 	}
 }
